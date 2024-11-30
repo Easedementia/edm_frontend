d6a9a60c744246d348ae0ee71fd0058160ad6cbf
Easedementia <it.easedementia@gmail.com>
2024-11-30 13:06:08 +0530

    login page ui corrections

--- src/Styles/UserLogin/UserLoginStyle.jsx
+++ src/Styles/UserLogin/UserLoginStyle.jsx
@@ -239,7 +239,7 @@ export const Label = styled.label`
 export const Input = styled.input`
     padding: 0.75rem;
     margin-bottom: 1rem;
-    border: 1px solid #f9fafb;
+    border: 1px solid #d9e1ea;
     border-radius: 10px;
     font-size: 15px;
     height: 45px;
