589a9e662524931747ec47796532de7644608cb8
Easedementia <it.easedementia@gmail.com>
2024-12-04 15:06:34 +0530

    privacy policy content

--- src/Components/Footer/PrivacyPolicy.jsx
+++ src/Components/Footer/PrivacyPolicy.jsx
@@ -21,20 +21,33 @@ const PrivacyPolicy = () => {
         <InstructionItem>
         <b>Information we collect</b>: The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information. If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide. When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.
         </InstructionItem>
+        
         <InstructionItem>
-            
-        </InstructionItem><b>How we use your information</b>: 
-        We use the information we collect in various ways, including to:
-        <br />
-            Provide, operate, and maintain our website
-            Improve, personalize, and expand our website
-            Understand and analyze how you use our website
-            Develop new products, services, features, and functionality
-            Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes
-            Send you emails
-            Find and prevent fraud
-            Log Files <br />
-            www.easedementia.com follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services’ analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users’ movement on the website, and gathering demographic information.
+            <b>How we use your information</b>: 
+            <p>We use the information we collect in various ways, including to:</p>
+            <ul>
+                <li>Provide, operate, and maintain our website</li>
+                <li>Improve, personalize, and expand our website</li>
+                <li>Understand and analyze how you use our website</li>
+                <li>Develop new products, services, features, and functionality</li>
+                <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+                <li>Send you emails</li>
+                <li>Find and prevent fraud</li>
+                <li>Log Files</li>
+            </ul>
+            <p>
+                www.easedementia.com follows a standard procedure of using log files.
+                These files log visitors when they visit websites. All hosting companies do this as a part
+                of hosting services’ analytics. The information collected by log files includes internet
+                protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time
+                stamp, referring/exit pages, and possibly the number of clicks. These are not linked to
+                any information that is personally identifiable. The purpose of the information is for
+                analyzing trends, administering the site, tracking users’ movement on the website, and
+                gathering demographic information.
+            </p>
+            </InstructionItem>
+
+        
 
         <InstructionItem>
             <b>Cookies and Web Beacons</b>: Like any other website, www.easedementia.com uses ’cookies’. These cookies are used to store information including visitors’ preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users’ experience by customizing our web page content based on visitors’ browser type and/or other information.
