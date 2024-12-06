6e921ebe533923125a3005f623e340a8df38cba3
Easedementia <it.easedementia@gmail.com>
2024-12-06 13:04:48 +0530

    usernavbar hamburger icon responsive style

--- src/Styles/Navbar/UserNavbarStyles.jsx
+++ src/Styles/Navbar/UserNavbarStyles.jsx
@@ -97,8 +97,8 @@ export const HamburgerIcon = styled.div`
   }
 
   img {
-    width: 30px;
-    height: 30px;
+    width: 80px;
+    height: 80px;
     background: none;
     padding: 0;
     margin: 0;
