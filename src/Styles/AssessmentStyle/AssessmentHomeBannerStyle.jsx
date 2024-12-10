271a7a68388c5f41cc8b6cb196027699afc1a337
Easedementia <it.easedementia@gmail.com>
2024-12-10 12:59:10 +0530

    responsive assessment banner style

--- src/Styles/AssessmentStyle/AssessmentHomeBannerStyle.jsx
+++ src/Styles/AssessmentStyle/AssessmentHomeBannerStyle.jsx
@@ -10,11 +10,35 @@ export const AssessmentContainer = styled.div`
     background-color: #f9fafb;
     font-family: 'Poppins', sans-serif;
     margin-top: 140px;
+
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+        margin-top: 100px;
+    }
+
+
+    @media (max-width: 480px) {
+        padding: 1rem;
+        margin-top: 80px;
+    }
 `;
 
 
 export const HeadingContainer = styled.div`
     max-width: 50%;
+
+
+    @media (max-width: 768px) {
+        max-width: 100%;
+        text-align: center;
+    }
+
+
+    @media (max-width: 480px) {
+        padding: 0 1rem;
+    }
 `;
 
 
@@ -24,6 +48,17 @@ export const MainHeading = styled.h1`
     margin: 0.5rem 0;
     text-align: left;
     font-weight: 700;
+
+
+    @media (max-width: 768px) {
+        text-align: center;
+        font-size: 36px;
+    }
+
+
+    @media (max-width: 480px) {
+        font-size: 28px;
+    }
 `;
 
 
@@ -32,6 +67,17 @@ export const SubHeading = styled.h2`
     font-weight: 350;
     color: #5517A8;
     text-align: left;
+
+
+    @media (max-width: 768px) {
+        text-align: center;
+        font-size: 20px;
+    }
+
+
+    @media (max-width: 480px) {
+        font-size: 18px;
+    }
 `;
 
 
@@ -40,12 +86,35 @@ export const Description = styled.p`
     color: #2f2e2e;
     text-align: left;
     max-width: 400px;
+
+
+    @media (max-width: 768px) {
+        text-align: center;
+        margin: 0 auto;
+        max-width: 90%;
+    }
+
+
+    @media (max-width: 480px) {
+        font-size: 14px;
+        line-height: 1.5;
+    }
 `;
 
 
 export const ButtonContainer = styled.div`
     margin-top: 20px;
     margin-right: 150px;
+
+
+    @media (max-width: 768px) {
+        margin-right: 0;
+    }
+
+
+    @media (max-width: 480px) {
+        margin-top: 15px;
+    }
 `;
 
 
@@ -73,6 +142,19 @@ export const AssessmentButton = styled(Link)`
         background-color: #6d22d1;
         color: #fff;
     }
+
+
+    @media (max-width: 768px) {
+        align-self: center;
+        margin-right: 0;
+    }
+
+
+    @media (max-width: 480px) {
+        width: 240px;
+        font-size: 14px;
+        padding: 10px 12px;
+    }
 `;
 
 
@@ -86,11 +168,28 @@ export const Icon = styled.img`
     &:hover {
         transform: scale(1.2); 
     }
+
+
+    @media (max-width: 480px) {
+        width: 50px;
+        height: 50px;
+    }
 `;
 
 
 export const ImageContainer = styled.div`
     max-width: 50%;
+
+
+    @media (max-width: 768px) {
+        max-width: 100%;
+        margin-top: 20px;
+    }
+
+
+    @media (max-width: 480px) {
+        margin-top: 15px;
+    }
 `;
 
 
