55bf9da342d244328d84607b0d840bb3a2563645
Easedementia <it.easedementia@gmail.com>
2024-12-11 16:03:27 +0530

    added navbarand footer in user signup page

--- src/Styles/UserSignup/UserSignupStyle.jsx
+++ src/Styles/UserSignup/UserSignupStyle.jsx
@@ -205,7 +205,7 @@ export const FormWrapper = styled.div`
   justify-content: space-between;
   overflow: visible;
   margin: auto;
-  margin-top: 50px;
+  margin-top: 80px;
 
 
   @media (min-width: 1024px) {
